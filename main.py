91f406679d90e3c70cfdad6196758677ed300443
brusbox86 <a-stroy@tut.by>
2021-03-06 19:17:20 +0300

    update

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 class Transport:
-    #  git commit == git push
+    def __init__(self, color, country, mark, weight, engine_volume, engine_type):
+        self.color = color
+        self.country = country
+        self.mark = mark
+        self.weight = weight
+        self.engine_volume = engine_volume
+        self.engine_type = engine_type
+
